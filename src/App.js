b7705b0f54ccb50356aaebf73a18c11dc62763f8
sumayea <sumayeablash@gmail.com>
2022-04-10 00:01:03 +0200

    deployed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import NavBar from './components/layout/NavBar';
+import AboutPage from './components/pages/AboutPage';
+import ContactPage from './components/pages/ContactPage';
+import  HomePage from './components/pages/HomePage';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import PageNotFound from './components/pages/PageNotFound';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <NavBar/>
+         <Routes>
+           <Route exact path='/' element ={<HomePage />} />
+           <Route exact path='/about' element ={<AboutPage />} />
+           <Route exact path='/contact' element ={<ContactPage />} />
+           <Route  path= "*" element ={<PageNotFound />} />
+         </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/AboutPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div className='container'>
+      <h1 className='py-2'> About Page</h1>
+      <p className='lead'> Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+     </p>
+
+     <p className='lead'> Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+      Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
